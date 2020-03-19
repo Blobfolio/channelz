42395e8ad2774a8551013f8aa236a0d1f0b1540c
Josh Stoik <josh@blobfolio.com>
2020-03-19 13:45:37 -0700

    first release

--- channelz/src/main.rs
+++ channelz/src/main.rs
@@ -1,7 +1,10 @@
 /*!
 # ChannelZ
 
-Generate static Brotli and Gzip encodings of a file or directory.
+Nothing but static…
+
+Use ChannelZ to generate maximally-compressed Gzip- and Brotli-encoded copies
+of a file or recurse a directory to do it for many files at once.
 */
 
 #![warn(missing_docs)]
@@ -15,6 +18,7 @@ Generate static Brotli and Gzip encodings of a file or directory.
 extern crate brotli;
 extern crate clap;
 extern crate flate2;
+extern crate rayon;
 extern crate regex;
 extern crate walkdir;
 
@@ -23,81 +27,57 @@ use clap::Shell;
 use flate2::Compression;
 use flate2::write::GzEncoder;
 use rayon::prelude::*;
+use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{copy, BufReader, SeekFrom};
-use std::io;
+use std::io::stdout;
 use std::path::{Path, PathBuf};
+use std::process::exit;
 use walkdir::WalkDir;
-use regex::Regex;
 
 
 
 fn main() -> Result<(), String> {
+	// Command line arguments.
 	let opts: clap::ArgMatches = menu()
 		.get_matches();
 
+	// Generate completions and exit.
 	if opts.is_present("completions") {
 		menu().gen_completions_to(
 			"channelz",
 			Shell::Bash,
-			&mut io::stdout()
+			&mut stdout()
 		);
-		std::process::exit(0);
+		exit(0);
 	}
 
-	let path = PathBuf::from(opts.value_of("path").unwrap_or("/none"));
-	let mut exts: Vec<String> = vec![
-		".css".to_string(),
-		".html".to_string(),
-		".ico".to_string(),
-		".js".to_string(),
-		".json".to_string(),
-		".mjs".to_string(),
-		".svg".to_string(),
-		".xml".to_string(),
-	];
+	// What path are we dealing with?
+	let path: PathBuf = PathBuf::from(opts.value_of("path").expect("A path is required."));
 
+	// Recurse a directory.
 	if path.is_dir() {
-		if let Some(x) = opts.values_of("ext") {
-			let mut raw: Vec<String> = x.filter_map(|y| {
-				let z: String = y.to_string()
-					.trim()
-					.to_lowercase()
-					.trim_start_matches(".")
-					.to_string();
-
-				if 0 < z.len() {
-					Some(format!(".{}", z))
-				}
-				else {
-					None
-				}
-			}).collect();
-
-			if 0 < raw.len() {
-				if 1 < raw.len() {
-					raw.sort();
-					raw.dedup();
-				}
-				exts = raw;
-			}
-		}
+		let (exts, exts_clean) = init_exts(&opts);
 
 		// Go ahead and clean.
 		if opts.is_present("clean") {
-			clean_dir(path.to_path_buf(), &exts);
+			path.channelz_clean(&exts_clean);
+		}
+
+		// Loop and compress!
+		if let Ok(paths) = path.channelz_find(&exts) {
+			paths.into_par_iter().for_each(|ref x| {
+				if let Err(_) = x.channelz_encode() {}
+			});
 		}
 	}
-
-	let exts: Regex = ext_regex(&exts);
-	if let Ok(paths) = file_list(path.to_path_buf(), &exts) {
-		let params = BrotliEncoderParams::default();
-
-		paths.into_par_iter().for_each(|x| {
-			if let Err(_) = encode(x.to_path_buf(), &params) {}
-		});
+	// Just hit one file.
+	else if path.is_file() {
+		if false == path.channelz_encode().is_ok() {
+			return Err("No files were compressed.".to_string());
+		}
 	}
+	// There's nothing to do!
 	else {
 		return Err("No files were compressed.".to_string());
 	}
@@ -105,103 +85,40 @@ fn main() -> Result<(), String> {
 	Ok(())
 }
 
-/// Clean Directory.
-fn clean_dir(path: PathBuf, exts: &Vec<String>) {
-	if path.is_dir() {
-		// Make Brotli/Gzip versions of the extensions provided.
-		let mut exts2: Vec<String> = vec![];
-		for i in exts {
-			exts2.push(format!("{}.br", i));
-			exts2.push(format!("{}.gz", i));
-		}
+/// Initialize extension patterns.
+fn init_exts(opts: &clap::ArgMatches) -> (Regex, Regex) {
+	// Default patterns.
+	let mut exts: Regex = Regex::new(r"(?i)\.(css|html|ico|js|json|mjs|svg|xml)$").unwrap();
+	let mut exts_clean: Regex = Regex::new(r"(?i)\.(css|html|ico|js|json|mjs|svg|xml)\.(br|gz)$").unwrap();
 
-		let exts2: Regex = ext_regex(&exts2);
-		for i in WalkDir::new(&path)
-			.follow_links(true)
-			.into_iter() {
-				if let Ok(path) = i {
-					let path = path.path();
-					if true == path.channelz_ext_one_of(&exts2) {
-						path.channelz_unlink();
-					}
-				}
+	// User-supplied patterns.
+	if let Some(x) = opts.values_of("ext") {
+		let ext: Regex = Regex::new(r"^[A-Za-z]+$").unwrap();
+
+		let mut raw: Vec<String> = x.filter_map(|y| {
+			match ext.is_match(y) {
+				true => Some(y.to_string().to_lowercase()),
+				false => None,
 			}
-	}
-}
+		}).collect();
 
-/// Encode file!
-fn encode(path: PathBuf, params: &BrotliEncoderParams) -> Result<(), String> {
-	// We can re-use the input.
-	let mut input = File::open(path.to_path_buf()).map_err(|e| e.to_string())?;
+		if false == raw.is_empty() {
+			let inner: String = if 1 < raw.len() {
+				raw.par_sort();
+				raw.dedup();
 
-	// Brotli business.
-	let mut output = path.channelz_create("br".to_string());
-	brotli::BrotliCompress(&mut input, &mut output, &params).map_err(|e| e.to_string())?;
+				raw.join("|")
+			}
+			else {
+				raw[0].clone()
+			};
 
-	// Rewind.
-	input.seek(SeekFrom::Start(0)).map_err(|e| e.to_string())?;
-	let mut input = BufReader::new(&input);
-
-	// Gzip business.
-	let output = path.channelz_create("gz".to_string());
-    let mut encoder = GzEncoder::new(output, Compression::new(9));
-    copy(&mut input, &mut encoder).map_err(|e| e.to_string())?;
-    encoder.finish().map_err(|e| e.to_string())?;
-
-	Ok(())
-}
-
-/// Extension Regex.
-fn ext_regex(exts: &Vec<String>) -> Regex {
-	let re: Regex = Regex::new(r"^[a-z\.]+$").unwrap();
-
-	let esc: Vec<String> = exts.into_iter().filter_map(|ref x| {
-		if re.is_match(x) {
-			Some(x.replace(".", "\\."))
-		}
-		else {
-			None
-		}
-	}).collect();
-
-	let esc = &format!("(?i)({})$", esc.join("|"));
-	Regex::new(esc).unwrap()
-}
-
-/// Come up with compressable files.
-fn file_list(path: PathBuf, exts: &Regex) -> Result<Vec<PathBuf>, String> {
-	// One file requires no recursion.
-	if path.is_file() {
-		if true == path.channelz_ext_one_of(&exts) {
-			return Ok(vec![path.to_path_buf()]);
-		}
-		else {
-			return Err("Invalid path.".into());
-		}
-	}
-	// If it's a directory, let's peek!
-	else if path.is_dir() {
-		let paths: Vec<PathBuf> = WalkDir::new(&path)
-			.follow_links(true)
-			.into_iter()
-			.filter_map(|x| match x {
-				Ok(path) => {
-					let path = path.path();
-					match path.channelz_ext_one_of(&exts) {
-						true => Some(path.to_path_buf()),
-						false => None,
-					}
-				},
-				_ => None,
-			})
-			.collect();
-
-		if 0 < paths.len() {
-			return Ok(paths);
+			exts = Regex::new(&format!("(?i)\\.({})$", inner)).unwrap();
+			exts_clean = Regex::new(&format!("(?i)\\.({})\\.(br|gz)$", inner)).unwrap();
 		}
 	}
 
-	Err("Invalid path.".into())
+	return (exts, exts_clean);
 }
 
 /// CLI Menu.
@@ -241,57 +158,105 @@ fn menu() -> clap::App<'static, 'static> {
 
 /// Path Helpers
 pub trait PathFuckery {
-	/// Append extension (correctly).
-	fn channelz_append_ext(&self, ext: String, clean: bool) -> PathBuf;
+	/// Clean directory.
+	fn channelz_clean(&self, exts: &Regex);
 
 	/// Create file.
 	fn channelz_create(&self, ext: String) -> File;
 
-	/// Has One Of Exts.
-	fn channelz_ext_one_of(&self, exts: &Regex) -> bool;
+	/// Encode file!
+	fn channelz_encode(&self) -> Result<(), String>;
+
+	/// Find files.
+	fn channelz_find(&self, exts: &Regex) -> Result<Vec<PathBuf>, String>;
 
 	/// Unlink.
 	fn channelz_unlink(&self) -> bool;
 }
 
 impl PathFuckery for Path {
-	/// Append extension (correctly).
-	fn channelz_append_ext(&self, ext: String, clean: bool) -> PathBuf {
-		let src = self.to_str().unwrap_or("");
-		let out = format!("{}.{}", src, ext);
-		let out = PathBuf::from(out);
-
-		if true == clean && out.is_file() {
-			out.channelz_unlink();
+	/// Clean Directory.
+	///
+	/// Recurse a directory to remove all existing *.br and *.gz files
+	/// matching the extension pattern.
+	fn channelz_clean(&self, exts: &Regex) {
+		if let Ok(paths) = self.channelz_find(&exts) {
+			paths.into_par_iter().for_each(|ref x| {
+				x.channelz_unlink();
+			});
 		}
-
-		return out;
 	}
 
 	/// Create file.
 	fn channelz_create(&self, ext: String) -> File {
-		let out = self.channelz_append_ext(ext, true);
+		let out: PathBuf = PathBuf::from(format!(
+			"{}.{}",
+			self.to_str().expect("Unable to create path"),
+			&ext
+		));
+
+		if out.is_file() {
+			out.channelz_unlink();
+		}
+
 		File::create(out).expect("That didn't work!")
 	}
 
-	/// Has One Of Exts.
-	fn channelz_ext_one_of(&self, exts: &Regex) -> bool {
-		if self.is_file() {
-			if let Some(name) = self.file_name() {
-				return exts.is_match(name.to_str().unwrap_or(""));
-			}
-		}
+	/// Encode file!
+	///
+	/// Generate Brotli and Gzip versions of a given file.
+	fn channelz_encode(&self) -> Result<(), String> {
+		// Load the full file contents as we'll need to reference it twice.
+		let data = std::fs::read(&self).expect("Unable to read file.");
 
-		return false;
+		// Brotli business.
+		let mut output = self.channelz_create("br".to_string());
+		let mut encoder = brotli::CompressorWriter::with_params(
+			&mut output,
+			4096,
+			&BrotliEncoderParams::default()
+		);
+		encoder.write_all(&data).unwrap();
+		encoder.flush().unwrap();
+
+		// Gzip business.
+		let output = self.channelz_create("gz".to_string());
+		let mut encoder = GzEncoder::new(output, Compression::new(9));
+		encoder.write_all(&data).unwrap();
+		encoder.finish().unwrap();
+
+		Ok(())
+	}
+
+	/// Find files.
+	fn channelz_find(&self, exts: &Regex) -> Result<Vec<PathBuf>, String> {
+		let paths: Vec<PathBuf> = WalkDir::new(&self)
+			.follow_links(true)
+			.into_iter()
+			.filter_map(|x| match x {
+				Ok(path) => {
+					if
+						path.file_type().is_file() &&
+						exts.is_match(path.file_name().to_str().unwrap_or(""))
+					{
+						Some(path.path().to_path_buf())
+					}
+					else {
+						None
+					}
+				},
+				_ => None,
+			})
+			.collect();
+
+		match paths.is_empty() {
+			false => Ok(paths),
+			true => Err("Invalid path.".into())
+		}
 	}
 
 	/// Unlink.
 	fn channelz_unlink(&self) -> bool {
-		if self.is_file() {
-			std::fs::remove_file(&self).is_ok()
-		}
-		else {
-			false
-		}
+		std::fs::remove_file(&self).is_ok()
 	}
 }
