51cd6726c86b9d0007c5f28d8f345247efef4dc4
Josh Stoik <josh@blobfolio.com>
2020-04-07 13:49:35 -0700

    update: fyi crate changes

--- channelz/src/main.rs
+++ channelz/src/main.rs
@@ -30,21 +30,16 @@ use compu::encoder::{
 	ZlibEncoder,
 };
 use fyi_core::{
+	arc::progress as parc,
 	Msg,
 	Prefix,
 	Progress,
-	progress_arc,
 	PROGRESS_CLEAR_ON_FINISH,
-	witcher,
-	traits::{
-		FYIPathIO,
-		FYIPathMIO,
-		FYIPathWalk,
-	},
+	traits::path::FYIPathIO,
+	Witch,
 };
 use rayon::prelude::*;
 use std::{
-	collections::HashSet,
 	fs::File,
 	path::PathBuf,
 	time::Instant,
@@ -57,27 +52,36 @@ fn main() -> Result<(), String> {
 	let opts: ArgMatches = menu::menu()
 		.get_matches();
 
-	let pattern = witcher::pattern_to_regex(r"(?i).+\.(css|x?html?|ico|m?js|json|svg|txt|xml|xsl)$");
-
 	// What path are we dealing with?
-	let paths: HashSet<PathBuf> = match opts.is_present("list") {
-		false => opts.values_of("path").unwrap()
-			.into_iter()
-			.filter_map(|x| Some(PathBuf::from(x)))
-			.collect::<HashSet<PathBuf>>()
-			.fyi_walk_filtered(&pattern),
-		true => PathBuf::from(opts.value_of("list").unwrap_or(""))
-			.fyi_walk_file_lines_hs(Some(pattern)),
+	let walk: Witch = match opts.is_present("list") {
+		false => {
+			let paths: Vec<PathBuf> = opts.values_of("path").unwrap()
+				.into_iter()
+				.filter_map(|x| Some(PathBuf::from(x)))
+				.collect();
+
+			Witch::new(
+				&paths,
+				Some(r"(?i).+\.(css|x?html?|ico|m?js|json|svg|txt|xml|xsl)$".to_string())
+			)
+		},
+		true => {
+			let path = PathBuf::from(opts.value_of("list").unwrap_or(""));
+			Witch::from_file(
+				&path,
+				Some(r"(?i).+\.(css|x?html?|ico|m?js|json|svg|txt|xml|xsl)$".to_string())
+			)
+		},
 	};
 
-	if paths.is_empty() {
+	if walk.is_empty() {
 		return Err("No encodable files were found.".to_string());
 	}
 
 	// With progress.
 	if opts.is_present("progress") {
 		let time = Instant::now();
-		let found: u64 = paths.len() as u64;
+		let found: u64 = walk.len() as u64;
 
 		{
 			let bar = Progress::new(
@@ -87,13 +91,13 @@ fn main() -> Result<(), String> {
 				found,
 				PROGRESS_CLEAR_ON_FINISH
 			);
-			let looper = progress_arc::looper(&bar, 60);
-			paths.par_iter().for_each(|ref x| {
-				progress_arc::add_working(&bar, &x);
+			let looper = parc::looper(&bar, 60);
+			walk.files().as_ref().par_iter().for_each(|x| {
+				parc::add_working(&bar, &x);
 				let _ = x.encode().is_ok();
-				progress_arc::update(&bar, 1, None, Some(x.to_path_buf()));
+				parc::update(&bar, 1, None, Some(x.to_path_buf()));
 			});
-			progress_arc::finish(&bar);
+			parc::finish(&bar);
 			looper.join().unwrap();
 		}
 
@@ -102,7 +106,7 @@ fn main() -> Result<(), String> {
 	}
 	// Without progress.
 	else {
-		paths.par_iter().for_each(|ref x| {
+		walk.files().as_ref().par_iter().for_each(|ref x| {
 			let _ = x.encode().is_ok();
 		});
 	}
