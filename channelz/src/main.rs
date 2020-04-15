f082db66bc6c92dfc3cf9f02ef37f385009ec955
Josh Stoik <josh@blobfolio.com>
2020-04-15 13:24:05 -0700

    misc: use Error::NoPaths

--- channelz/src/main.rs
+++ channelz/src/main.rs
@@ -70,7 +70,7 @@ fn main() -> Result<()> {
 	};
 
 	if walk.is_empty() {
-		return Err(Error::Other("No encodable files were found.".to_string()));
+		return Err(Error::NoPaths("encodable files".into()));
 	}
 
 	// With progress.
