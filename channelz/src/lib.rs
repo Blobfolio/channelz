ed32a43456e13ad307ceb58e3bd9ba97d4c329f5
Josh Stoik <josh@blobfolio.com>
2020-06-14 12:25:41 -0700

    perf: move remaining sanity/IO work to threads

--- channelz/src/lib.rs
+++ channelz/src/lib.rs
@@ -12,6 +12,7 @@ use compu::{
 	},
 };
 use std::{
+	ffi::OsString,
 	fs::{
 		self,
 		File,
@@ -23,38 +24,52 @@ use std::{
 
 // Do the dirty work!
 pub fn encode_path(path: &PathBuf) {
-	if let Ok(data) = &fs::read(path) {
-		if ! data.is_empty() {
-			let _ = rayon::join(
-				|| encode_br(path, data),
-				|| encode_gz(path, data),
-			);
+	let _ = rayon::join(
+		|| encode_br(path),
+		|| encode_gz(path),
+	);
+}
+
+#[allow(unused_must_use)]
+pub fn encode_br(path: &PathBuf) {
+	// It is more efficient to calculate the output path from OsString than
+	// Path or PathBuf since every goddamn Path join/concat-type method adds a
+	// separator.
+	let mut out_path: OsString = OsString::from(path);
+	out_path.reserve_exact(3);
+	out_path.push(".br");
+
+	// Create the output file.
+	if let Ok(mut output) = File::create(&out_path) {
+		let mut writer = Compressor::new(BrotliEncoder::default(), &mut output);
+
+		// Write the data! If nothing is written because of failure or general
+		// emptiness, try to delete the file we just created.
+		if 0 == writer.push(&fs::read(path).unwrap_or_default(), EncoderOp::Finish).unwrap_or_default() {
+			drop(output);
+			fs::remove_file(out_path);
 		}
 	}
 }
 
 #[allow(unused_must_use)]
-pub fn encode_br(path: &PathBuf, data: &[u8]) {
-	if let Ok(mut output) = File::create({
-		let mut p = path.to_path_buf().into_os_string();
-		p.reserve_exact(3);
-		p.push(".br");
-		p
-	}) {
-		let mut writer = Compressor::new(BrotliEncoder::default(), &mut output);
-		writer.push(data, EncoderOp::Finish);
-	}
-}
+pub fn encode_gz(path: &PathBuf) {
+	// It is more efficient to calculate the output path from OsString than
+	// Path or PathBuf since every goddamn Path join/concat-type method adds a
+	// separator.
+	let mut out_path: OsString = OsString::from(path);
+	out_path.reserve_exact(3);
+	out_path.push(".gz");
 
-#[allow(unused_must_use)]
-pub fn encode_gz(path: &PathBuf, data: &[u8]) {
-	if let Ok(mut output) = File::create({
-		let mut p = path.to_path_buf().into_os_string();
-		p.reserve_exact(3);
-		p.push(".gz");
-		p
-	}) {
+	// Create the output file.
+	if let Ok(mut output) = File::create(&out_path) {
 		let mut writer = Compressor::new(ZlibEncoder::default(), &mut output);
-		writer.push(data, EncoderOp::Finish);
+
+		// Write the data! If nothing is written because of failure or general
+		// emptiness, try to delete the file we just created.
+		if 0 == writer.push(&fs::read(path).unwrap_or_default(), EncoderOp::Finish).unwrap_or_default() {
+			drop(output);
+			fs::remove_file(out_path);
+		}
 	}
 }
