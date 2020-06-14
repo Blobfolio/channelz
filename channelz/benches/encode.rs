c2215a0ac5eabe26dd1f251c940a60c9b68ec9c9
Josh Stoik <josh@blobfolio.com>
2020-06-14 12:24:01 -0700

    add: encode_gz bench

--- channelz/benches/encode.rs
+++ channelz/benches/encode.rs
@@ -34,12 +34,25 @@ fn encode_br(c: &mut Criterion) {
 	].iter() {
 		assert!(path.is_file(), "Invalid file: {:?}", path);
 
-		let data = std::fs::read(&path).unwrap();
 		group.bench_function(format!("{:?}", path), move |b| {
-			b.iter(|| channelz::encode_br(path, &data))
+			b.iter(|| channelz::encode_br(path))
 		});
+	}
 
-		assert!(PathBuf::from([path.to_str().unwrap(), ".br"].concat()).is_file(), "No BR created!");
+	group.finish();
+}
+
+fn encode_gz(c: &mut Criterion) {
+	let mut group = c.benchmark_group("channelz::encode_gz");
+
+	for path in [
+		PathBuf::from("../test/assets/favicon.svg"),
+	].iter() {
+		assert!(path.is_file(), "Invalid file: {:?}", path);
+
+		group.bench_function(format!("{:?}", path), move |b| {
+			b.iter(|| channelz::encode_gz(path))
+		});
 	}
 
 	group.finish();
@@ -51,5 +64,6 @@ criterion_group!(
 	benches,
 	encode_path,
 	encode_br,
+	encode_gz,
 );
 criterion_main!(benches);
