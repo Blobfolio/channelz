37bc8dcf6e87d701a74450b2fc9e921efac04438
Josh Stoik <josh@blobfolio.com>
2020-08-09 21:39:47 -0700

    misc: switch to libdeflater for Gzip

--- channelz/src/lib.rs
+++ channelz/src/lib.rs
@@ -27,15 +27,8 @@
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::unknown_clippy_lints)]
 
-use compu::{
-	compressor::write::Compressor,
-	encoder::{
-		Encoder,
-		EncoderOp,
-		BrotliEncoder,
-		ZlibEncoder,
-	},
-};
+
+
 use std::{
 	ffi::{
 		OsStr,
@@ -45,24 +38,42 @@ use std::{
 		self,
 		File,
 	},
-	os::unix::ffi::OsStringExt,
+	io::Write,
+	os::unix::ffi::{
+		OsStrExt,
+		OsStringExt,
+	},
 	path::PathBuf,
+	sync::Arc,
 };
 
 
 
 /// Do the dirty work!
 pub fn encode_path(path: &PathBuf) {
-	let _ = rayon::join(
-		|| encode_br(path),
-		|| encode_gz(path),
-	);
+	let a_data: Arc<Vec<u8>> = Arc::from(fs::read(path).unwrap_or_default());
+	if ! a_data.is_empty() {
+		rayon::join(
+			|| encode_br(path, &a_data),
+			|| encode_gz(path, &a_data),
+		);
+	}
 }
 
 #[allow(trivial_casts)] // It is better this way.
 #[allow(unused_must_use)] // We don't care.
 /// Encode `Brotli`.
-pub fn encode_br(path: &PathBuf) {
+pub fn encode_br(path: &PathBuf, data: &Arc<Vec<u8>>) {
+	use compu::{
+		compressor::write::Compressor,
+		encoder::{
+			Encoder,
+			EncoderOp,
+			BrotliEncoder,
+		},
+	};
+
+	// Calculate the output path.
 	let out_path: OsString = unsafe {
 		OsString::from_vec(
 			[
@@ -76,9 +87,9 @@ pub fn encode_br(path: &PathBuf) {
 	if let Ok(mut output) = File::create(&out_path) {
 		let mut writer = Compressor::new(BrotliEncoder::default(), &mut output);
 
-		// Write the data! If nothing is written because of failure or general
-		// emptiness, try to delete the file we just created.
-		if 0 == writer.push(&fs::read(path).unwrap_or_default(), EncoderOp::Finish).unwrap_or_default() {
+		// Stream-write to the file. If it fails, we'll need to delete the file
+		// we just created.
+		if 0 == writer.push(data, EncoderOp::Finish).unwrap_or_default() {
 			drop(output);
 			fs::remove_file(out_path);
 		}
@@ -86,27 +97,38 @@ pub fn encode_br(path: &PathBuf) {
 }
 
 #[allow(trivial_casts)] // It is better this way.
-#[allow(unused_must_use)] // We don't care.
 /// Encode `GZip`.
-pub fn encode_gz(path: &PathBuf) {
-	let out_path: OsString = unsafe {
-		OsString::from_vec(
-			[
+pub fn encode_gz(path: &PathBuf, data: &Arc<Vec<u8>>) {
+	use libdeflater::{
+		CompressionLvl,
+		Compressor,
+	};
+
+	// This compresses to memory.
+	let compressed: Vec<u8> = {
+		let mut writer = Compressor::new(CompressionLvl::best());
+		let mut tmp = Vec::new();
+		tmp.resize(writer.gzip_compress_bound(data.len()), 0);
+
+		match writer.gzip_compress(data, &mut tmp) {
+			Ok(len) if len > 0 => {
+				tmp.resize(len, 0);
+				tmp
+			},
+			_ => { return; }
+		}
+	};
+
+	// Write what needs writing.
+	if let Ok(mut output) = File::create(unsafe {
+		OsStr::from_bytes(
+			&[
 				&*(path.as_os_str() as *const OsStr as *const [u8]),
 				b".gz",
 			].concat()
 		)
-	};
-
-	// Create the output file.
-	if let Ok(mut output) = File::create(&out_path) {
-		let mut writer = Compressor::new(ZlibEncoder::default(), &mut output);
-
-		// Write the data! If nothing is written because of failure or general
-		// emptiness, try to delete the file we just created.
-		if 0 == writer.push(&fs::read(path).unwrap_or_default(), EncoderOp::Finish).unwrap_or_default() {
-			drop(output);
-			fs::remove_file(out_path);
-		}
+	}) {
+		output.write_all(&compressed).unwrap();
+		output.flush().unwrap();
 	}
 }
