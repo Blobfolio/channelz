b959b40d31e9b21277cd0cd4015207ab1b21a0cb
Josh Stoik <josh@blobfolio.com>
2020-08-10 13:36:21 -0700

    misc: update benches

--- channelz/benches/encode.rs
+++ channelz/benches/encode.rs
@@ -15,43 +15,13 @@ fn encode_path(c: &mut Criterion) {
 	let mut group = c.benchmark_group("channelz::encode_path");
 
 	for path in [
-		PathBuf::from("../test/assets/favicon.svg"),
+		PathBuf::from("../test/assets/core.css").canonicalize().expect("Missing file."),
+		PathBuf::from("../test/assets/core.min.js").canonicalize().expect("Missing file."),
+		PathBuf::from("../test/assets/favicon.svg").canonicalize().expect("Missing file."),
+		PathBuf::from("../test/assets/index.html").canonicalize().expect("Missing file."),
 	].iter() {
-		assert!(path.is_file(), "Invalid file: {:?}", path);
 		group.bench_function(format!("{:?}", path), move |b| {
 			b.iter(|| channelz::encode_path(path))
-		});
-	}
-
-	group.finish();
-}
-
-fn encode_br(c: &mut Criterion) {
-	let mut group = c.benchmark_group("channelz::encode_br");
-
-	for path in [
-		PathBuf::from("../test/assets/favicon.svg"),
-	].iter() {
-		assert!(path.is_file(), "Invalid file: {:?}", path);
-
-		group.bench_function(format!("{:?}", path), move |b| {
-			b.iter(|| channelz::encode_br(path))
-		});
-	}
-
-	group.finish();
-}
-
-fn encode_gz(c: &mut Criterion) {
-	let mut group = c.benchmark_group("channelz::encode_gz");
-
-	for path in [
-		PathBuf::from("../test/assets/favicon.svg"),
-	].iter() {
-		assert!(path.is_file(), "Invalid file: {:?}", path);
-
-		group.bench_function(format!("{:?}", path), move |b| {
-			b.iter(|| channelz::encode_gz(path))
 		});
 	}
 
@@ -63,7 +33,5 @@ fn encode_gz(c: &mut Criterion) {
 criterion_group!(
 	benches,
 	encode_path,
-	encode_br,
-	encode_gz,
 );
 criterion_main!(benches);
