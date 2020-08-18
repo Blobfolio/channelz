334ff02d034fe669875d92d644284d1491a7faf1
Josh Stoik <josh@blobfolio.com>
2020-08-17 18:35:52 -0700

    misc: update FYI

--- channelz/src/main.rs
+++ channelz/src/main.rs
@@ -33,94 +33,46 @@ of a file or recurse a directory to do it for many files at once.
 #![allow(clippy::unknown_clippy_lints)]
 
 use channelz::encode_path;
+use fyi_menu::Argue;
 use fyi_msg::MsgKind;
 use fyi_witcher::{
 	Witcher,
 	WITCHING_QUIET,
 	WITCHING_SUMMARIZE,
 };
-use std::{
-	io::{
-		self,
-		Write,
-	},
-	ops::Range,
-};
 
 
 
 fn main() {
-	let args = fyi_menu::parse_env_args(fyi_menu::FLAG_ALL);
-	let (flags, rg, list) = parse_args(&args);
+	// Parse CLI arguments.
+	let args = Argue::new()
+		.with_any()
+		.with_version("ChannelZ")
+		.with_help(helper)
+		.with_list();
+
+	let mut flags: u8 = WITCHING_QUIET | WITCHING_SUMMARIZE;
+	if args.switch2("-p", "--progress") {
+		flags &= ! WITCHING_QUIET;
+	}
 
 	// Put it all together!
 	Witcher::default()
 		.with_regex(r"(?i).+\.(css|eot|x?html?|ico|m?js|json|otf|rss|svg|ttf|txt|xml|xsl)$")
-		.with(&args[rg], list)
+		.with_paths(args.args())
 		.into_witching()
 		.with_flags(flags)
 		.with_title(MsgKind::new("ChannelZ", 199).into_msg("Reticulating splines\u{2026}"))
 		.run(encode_path);
 }
 
-
-
-#[allow(clippy::reversed_empty_ranges)] // Witcher will print an error for us.
-#[allow(clippy::range_plus_one)] // We need a consistent return type!
-/// Parse Options.
-///
-/// Returns a tuple containing the flags, path range, and whether or not it is
-/// a list.
-fn parse_args(args: &[String]) -> (u8, Range<usize>, bool) {
-	let mut flags: u8 = WITCHING_QUIET | WITCHING_SUMMARIZE;
-	let mut list: usize = 0;
-
-	// Run through the arguments to see what we've got going on!
-	let mut idx: usize = 0;
-	let len: usize = args.len();
-	while idx < len {
-		match args[idx].as_str() {
-			"-h" | "--help" => {
-				_help();
-				std::process::exit(0);
-			},
-			"-V" | "--version" => {
-				_version();
-				std::process::exit(0);
-			},
-			"-p" | "--progress" => {
-				flags &= ! WITCHING_QUIET;
-				idx += 1;
-			},
-			"-l" | "--list" =>
-				if idx + 1 < len {
-					list = idx + 1;
-					idx += 2;
-				}
-				else { idx += 1 },
-			_ => { break; }
-		}
-	}
-
-	// What paths are we dealing with?
-	(
-		flags,
-		match list {
-			0 if idx < args.len() => idx..args.len(),
-			0 => 0..0,
-			x => x..x+1,
-		},
-		0 != list
-	)
-}
-
-
-
 #[cfg(not(feature = "man"))]
 #[cold]
 /// Print Help.
-fn _help() {
-	io::stdout().write_fmt(format_args!(
+fn helper(_: Option<&str>) {
+	use std::io::Write;
+
+	std::io::stdout().write_fmt(format_args!(
 		r"
                   ,.
                  (\(\)
@@ -144,34 +96,25 @@ fn _help() {
 	)).unwrap();
 }
 
-
-
 #[cfg(feature = "man")]
 #[cold]
 /// Print Help.
 ///
 /// This is a stripped-down version of the help screen made specifically for
 /// `help2man`, which gets run during the Debian package release build task.
-fn _help() {
-	io::stdout().write_all(&[
-		b"ChannelZ ",
-		env!("CARGO_PKG_VERSION").as_bytes(),
-		b"\n",
-		env!("CARGO_PKG_DESCRIPTION").as_bytes(),
-		b"\n\n",
-		include_bytes!("../misc/help.txt"),
-		b"\n",
-	].concat()).unwrap();
-}
+fn helper(_: Option<&str>) {
+	use std::io::Write;
 
+	let writer = std::io::stdout();
+	let mut handle = writer.lock();
 
+	handle.write_all(b"ChannelZ ").unwrap();
+	handle.write_all(env!("CARGO_PKG_VERSION").as_bytes()).unwrap();
+	handle.write_all(b"\n").unwrap();
+	handle.write_all(env!("CARGO_PKG_DESCRIPTION").as_bytes()).unwrap();
+	handle.write_all(b"\n\n").unwrap();
+	handle.write_all(include_bytes!("../misc/help.txt")).unwrap();
+	handle.write_all(b"\n").unwrap();
 
-#[cold]
-/// Print version and exit.
-fn _version() {
-	io::stdout().write_all(&[
-		b"ChannelZ ",
-		env!("CARGO_PKG_VERSION").as_bytes(),
-		b"\n"
-	].concat()).unwrap();
+	handle.flush().unwrap();
 }
