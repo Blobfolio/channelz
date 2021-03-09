8bfb50d455542cc0aa3226642a0b448e9ee425e7
Josh Stoik <josh@blobfolio.com>
2021-03-09 11:22:12 -0800

    misc: set up error type, tweak flow

--- channelz_core/src/lib.rs
+++ channelz_core/src/lib.rs
@@ -30,6 +30,7 @@
 use std::{
 	convert::TryFrom,
 	ffi::OsStr,
+	fmt,
 	fs::{
 		self,
 		File,
@@ -58,6 +59,46 @@ where P: AsRef<Path> {
 
 
 
+#[derive(Debug, Copy, Clone)]
+/// # Error.
+pub enum ChannelZError {
+	/// # Empty file.
+	EmptyFile,
+	/// # Unable to encode file.
+	Encode,
+	/// # No compression savings.
+	NoCompression,
+	/// # Unable to read file.
+	Read,
+	/// # Unable to write file.
+	Write,
+}
+
+impl fmt::Display for ChannelZError {
+	#[inline]
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		f.write_str(self.as_str())
+	}
+}
+
+impl std::error::Error for ChannelZError {}
+
+impl ChannelZError {
+	#[must_use]
+	/// # As Str.
+	pub const fn as_str(self) -> &'static str {
+		match self {
+			Self::Encode => "Unable to encode the file.",
+			Self::EmptyFile => "The file is empty.",
+			Self::NoCompression => "No compression savings were possible.",
+			Self::Read => "Unable to read the file.",
+			Self::Write => "Unable to save the file.",
+		}
+	}
+}
+
+
+
 #[derive(Debug)]
 /// # `ChannelZ`
 ///
@@ -69,26 +110,24 @@ pub struct ChannelZ {
 }
 
 impl TryFrom<&PathBuf> for ChannelZ {
-	type Error = ();
+	type Error = ChannelZError;
 
 	#[allow(trivial_casts)] // This is how `std::path::PathBuf` does it.
 	fn try_from(src: &PathBuf) -> Result<Self, Self::Error> {
 		// Read the file.
-		if let Ok(raw) = fs::read(src) {
-			let len = raw.len();
-			if 0 != len {
-				return Ok(Self {
-					raw: raw.into_boxed_slice(),
-					buf: Vec::with_capacity(len),
-					dst: [
-						unsafe { &*(src.as_os_str() as *const OsStr as *const [u8]) },
-						b".br",
-					].concat().into_boxed_slice(),
-				});
-			}
+		let raw = fs::read(src).map_err(|_| ChannelZError::Read)?.into_boxed_slice();
+		if raw.is_empty() {
+			return Err(ChannelZError::EmptyFile);
 		}
 
-		Err(())
+		Ok(Self {
+			buf: Vec::with_capacity(raw.len()),
+			raw,
+			dst: [
+				unsafe { &*(src.as_os_str() as *const OsStr as *const [u8]) },
+				b".br",
+			].concat().into_boxed_slice(),
+		})
 	}
 }
 
@@ -96,12 +135,12 @@ impl ChannelZ {
 	#[inline]
 	/// # Encode!
 	pub fn encode(&mut self) {
-		self.encode_br();
-		self.encode_gz();
+		if self.encode_br().is_err() { self.delete_if(); }
+		if self.encode_gz().is_err() { self.delete_if(); }
 	}
 
 	/// # Encode Brotli.
-	fn encode_br(&mut self) {
+	fn encode_br(&mut self) -> Result<(), ChannelZError> {
 		use compu::{
 			compressor::write::Compressor,
 			encoder::{
@@ -112,18 +151,15 @@ impl ChannelZ {
 		};
 
 		let mut writer = Compressor::new(BrotliEncoder::default(), &mut self.buf);
-		if let Ok(x) = writer.push(&self.raw, EncoderOp::Finish) {
-			if 0 < x && x < self.raw.len() {
-				self.write();
-				return;
-			}
-		}
+		let len: usize = writer.push(&self.raw, EncoderOp::Finish)
+			.map_err(|_| ChannelZError::Encode)?;
 
-		self.delete_if();
+		if 0 < len && len < self.raw.len() { self.write() }
+		else { Err(ChannelZError::NoCompression) }
 	}
 
 	/// # Encode Gzip.
-	fn encode_gz(&mut self) {
+	fn encode_gz(&mut self) -> Result<(), ChannelZError> {
 		use libdeflater::{
 			CompressionLvl,
 			Compressor,
@@ -139,17 +175,17 @@ impl ChannelZ {
 			self.dst[len - 1] = b'z';
 		}
 
-		if let Ok(x) = writer.gzip_compress(&self.raw, &mut self.buf) {
-			if 0 < x && x < self.raw.len() {
-				// Libdeflater does not automatically truncate the buffer to
-				// the payload size.
-				self.buf.truncate(x);
-				self.write();
-				return;
-			}
-		}
+		let len: usize = writer.gzip_compress(&self.raw, &mut self.buf)
+			.map_err(|_| ChannelZError::Encode)?;
 
-		self.delete_if();
+		if 0 < len && len < self.raw.len() {
+			// Libdeflater does not automatically truncate the buffer to the
+			// final payload size, so we need to do that before trying to write
+			// the data to a file.
+			self.buf.truncate(len);
+			self.write()
+		}
+		else { Err(ChannelZError::NoCompression) }
 	}
 
 	#[cold]
@@ -157,6 +193,11 @@ impl ChannelZ {
 	///
 	/// We probably don't need to explicitly check the file exists, but it is
 	/// unclear how the underlying `unlink()` varies from system to system.
+	///
+	/// This method returns no result and suppresses any errors encountered as
+	/// there's not really anything more to be done. If the file doesn't exist,
+	/// it doesn't need to be deleted; if it does and can't be deleted, well,
+	/// we tried.
 	fn delete_if(&self) {
 		let path = PathBuf::from(OsStr::from_bytes(&self.dst));
 		if path.exists() {
@@ -167,9 +208,9 @@ impl ChannelZ {
 	/// # Write Result.
 	///
 	/// Write the buffer to an actual file.
-	fn write(&self) {
-		// The result doesn't matter. It'll work or it won't.
-		let _res = File::create(OsStr::from_bytes(&self.dst))
-			.and_then(|mut file| file.write_all(&self.buf).and_then(|_| file.flush()));
+	fn write(&self) -> Result<(), ChannelZError> {
+		File::create(OsStr::from_bytes(&self.dst))
+			.and_then(|mut file| file.write_all(&self.buf).and_then(|_| file.flush()))
+			.map_err(|_| ChannelZError::Write)
 	}
 }
