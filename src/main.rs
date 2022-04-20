cfc39acf2ce928483fb087a6049801fb4fe4d17f
Josh Stoik <josh@blobfolio.com>
2022-04-19 21:58:48 -0700

    misc: update fyi_msg

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,7 @@ fn main() {
 }
 
 #[inline]
+#[allow(clippy::cast_possible_truncation)] // It fits.
 /// # Actual Main.
 fn _main() -> Result<(), ArgyleError> {
 	// Parse CLI arguments.
@@ -103,11 +104,13 @@ fn _main() -> Result<(), ArgyleError> {
 
 	// Put it all together!
 	let paths: Vec<PathBuf> =
+		// Pull anything that isn't already br/gz.
 		if args.switch(b"--force") {
 			Dowser::default()
 				.with_paths(args.args_os())
 				.into_vec(|p| ! ext::match_br_gz(p.as_os_str().as_bytes()))
 		}
+		// Pull only supported files.
 		else {
 			Dowser::default()
 				.with_paths(args.args_os())
@@ -118,15 +121,16 @@ fn _main() -> Result<(), ArgyleError> {
 		return Err(ArgyleError::Custom("No encodeable files were found."));
 	}
 
-	// Should we show progress as we go?
-	let mut progress = args.switch2(b"-p", b"--progress");
-
-	#[cfg(any(target_pointer_width = "64", target_pointer_width = "128"))]
-	if progress && 4_294_967_295 < paths.len()  {
-		Msg::warning("Progress can't be displayed when there are more than 4,294,967,295 files.")
-			.print();
-		progress = false;
-	}
+	// Should we show progress?
+	let progress =
+		if args.switch2(b"-p", b"--progress") {
+			if paths.len() <= Progless::MAX_TOTAL { true }
+			else {
+				Msg::warning(Progless::MAX_TOTAL_ERROR).print();
+				false
+			}
+		}
+		else { false };
 
 	// Watch for SIGINT so we can shut down cleanly.
 	let killed = Arc::from(AtomicBool::new(false));
@@ -138,22 +142,17 @@ fn _main() -> Result<(), ArgyleError> {
 	// Sexy run-through.
 	if progress {
 		// Boot up a progress bar.
-		let progress = Progless::try_from(paths.len())
+		let progress = Progless::try_from(paths.len() as u32)
 			.unwrap()
-			.with_title(Some(Msg::custom("ChannelZ", 199, "Reticulating splines\u{2026}")));
+			.with_reticulating_splines("ChannelZ");
 
 		let size_src = AtomicU64::new(0);
 		let size_br = AtomicU64::new(0);
 		let size_gz = AtomicU64::new(0);
 
 		// Process!
-		let killed2 = AtomicBool::new(false);
 		paths.par_iter().for_each(|x| {
-			if killed.load(SeqCst) {
-				if killed2.compare_exchange(false, true, SeqCst, SeqCst).is_ok() {
-					progress.set_title(Some(Msg::warning("Early shutdown in progress.")));
-				}
-			}
+			if killed.load(SeqCst) { progress.sigint(); }
 			else {
 				let tmp = x.to_string_lossy();
 				progress.add(&tmp);
