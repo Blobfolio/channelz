f22da808e63c051749ff8f1eee4c56e28b28ac0d
Josh Stoik <josh@blobfolio.com>
2025-02-25 13:55:14 -0800

    bump: dowser 0.12

--- src/main.rs
+++ src/main.rs
@@ -177,10 +177,9 @@ fn main__() -> Result<(), ChannelZError> {
 	}
 
 	// Put it all together!
-	let mut paths: Vec<PathBuf> = paths.into_vec_filtered(
-		if force { find_all }
-		else { find_default }
-	);
+	let mut paths: Vec<PathBuf> =
+		if force { paths.filter(find_all).collect() }
+		else { paths.filter(find_default).collect() };
 	let total = NonZeroUsize::new(paths.len()).ok_or(ChannelZError::NoFiles)?;
 	paths.sort();
 
@@ -319,6 +318,7 @@ fn crunch_quiet(rx: &Receiver::<&Path>, kinds: u8) -> ThreadTotals {
 }
 
 #[cold]
+#[expect(clippy::ptr_arg, reason = "Needs to match filter() signature.")]
 /// # Find Non-GZ/BR.
 ///
 /// This is a callback for `Dowser`. That library ensures the paths passed will
@@ -326,8 +326,9 @@ fn crunch_quiet(rx: &Receiver::<&Path>, kinds: u8) -> ThreadTotals {
 ///
 /// For this variation, everything is fair game so long as it isn't already
 /// `gz`/`br`-encoded.
-fn find_all(p: &Path) -> bool { ! ext::match_encoded(p.as_os_str().as_bytes()) }
+fn find_all(p: &PathBuf) -> bool { ! ext::match_encoded(p.as_os_str().as_bytes()) }
 
+#[expect(clippy::ptr_arg, reason = "Needs to match filter() signature.")]
 /// # Find Default.
 ///
 /// This is a callback for `Dowser`. That library ensures the paths passed will
@@ -335,4 +336,4 @@ fn find_all(p: &Path) -> bool { ! ext::match_encoded(p.as_os_str().as_bytes()) }
 ///
 /// For this variation, we're looking for all the hard-coded "default" types.
 /// Refer to the main documentation or help screen for that list.
-fn find_default(p: &Path) -> bool { ext::match_extension(p.as_os_str().as_bytes()) }
+fn find_default(p: &PathBuf) -> bool { ext::match_extension(p.as_os_str().as_bytes()) }
